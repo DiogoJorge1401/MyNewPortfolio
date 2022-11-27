e6477b25ea60a88094e93efb48539d6c669376fd
Diogo Jorge <diogojorge1401@gmail.com>
2022-11-27 15:19:06 -0300

    chore: fix class name

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -54,7 +54,7 @@ export const Banner = () => {
           data-aos-easing="ease-in-out"
         >
           <Col xs={12} md={7} xl={7} className="leftColBanner">
-            <span className="taglinOe">Welcome to My Portfolio</span>
+            <span className="tagline">Welcome to My Portfolio</span>
 
             <h1>
               {"Hi I'm Diogo Jorge "}
